89b399bcaf32a14ca6aa31c0754bc1bafa65fd34
Yan Carvalho <yancarvalhodev@gmail.com>
2026-05-22 19:32:20 -0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,7 @@ export default defineConfig({
   trailingSlash: "never",
   // build: {
   //   inlineStylesheets: "always",
-  },
+  // },
   vite: {
     plugins: [tailwindcss()],
     resolve: {
